60bfce5a7766018f4f796b838522d9a894d31cf9
semihYILMAZ37 <117524189+semihYILMAZ37@users.noreply.github.com>
2022-11-12 22:30:17 +0300

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -1,9 +1,41 @@
-//C++ varsayılan ayarda Türkçe karakter yazdırmıyor. Destekleyen standart kütüphaneyi eklememiz gerekiyor.
-
-#include <locale.h> // veya C++ için <clocale>
+#include <iostream>
+#include <locale.h>
 int main() {
-  setlocale(LC_ALL, "Turkish");
-  printf("Türkçe karakter yazdırmak için örnek kod\n");
-  printf("setlocale fonksiyonu olmadan çalıştırınca ekrana saçmasapan bir yazı bastırır.\n");
-  return 0;
+	int girdi,birler,onlar,yuzler;
+	std::string birlerbasamagi[] = {"", "bir", "iki", "üç", "dört", "beş", "altı", "yedi", "sekiz", "dokuz"};
+	std::string onlarbasamagi[] = {"", "on", "yirmi", "otuz", "kırk", "elli", "altmış", "yetmiş", "seksen", "doksan"};
+	setlocale(LC_ALL, "Turkish");
+  	std::cout<<"1 ile 999 arasında bir tam sayı giriniz.";
+	std::cin>>girdi;
+	birler = girdi % 10;
+	onlar = (girdi/10)%10;
+	yuzler = (girdi/100)%10;
+	switch(yuzler){
+		case 0:
+			switch(onlar){
+				case 0:
+					switch(birler){
+						case 0:
+							
+							break;
+						default:
+							std::cout<<birlerbasamagi[birler];
+							break;
+					}
+					
+					break;
+				default:
+					std::cout<<onlarbasamagi[onlar]<<birlerbasamagi[birler];
+					break;
+				}
+	
+			break;
+		case 1:
+			std::cout<<"yüz"<<onlarbasamagi[onlar]<<birlerbasamagi[birler];
+			break;
+		default:
+			std::cout<<birlerbasamagi[yuzler]<<"yüz"<<onlarbasamagi[onlar]<<birlerbasamagi[birler];
+			break;
+	}
+	return 0;
 }
